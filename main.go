98374b03550027a534f890e06f3dbb451d635b9c
nyc4m <baptiste.prunot@utt.fr>
2019-05-19 22:44:20 +0700

    Change output name + text defaulted to false

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func main() {
 	var translated string
 
 	flag.StringVar(&toTranslate, "input", "hello world", "string that should be convert to bob retarded language")
-	flag.StringVar(&outputPath, "out", "./bob", "string indicating where the file image should be generated")
-	flag.BoolVar(&toText, "text", true, "if true, will only ouptut to text")
+	flag.StringVar(&outputPath, "out", "./bob.png", "string indicating where the file image should be generated")
+	flag.BoolVar(&toText, "text", false, "if true, will only ouptut to text")
 
 	flag.Parse()
 
