966bdde54bced78d8b354808cc4f9f55b5e89aca
nyc4m <baptiste.prunot@utt.fr>
2019-05-29 15:23:34 +0700

    Convert incoming string to lower
    so Aaa becomes AaA instead of AAA

--- text/string-converter.go
+++ text/string-converter.go
@@ -7,7 +7,7 @@ import (
 //ToBobRetardedString Translate to a bob retarded string
 //so 'Hello world', becomes 'HeLlO wOrLd'
 func ToBobRetardedString(cleverSentence string) (retardedSentence string) {
-	characters := strings.Split(cleverSentence, "")
+	characters := strings.Split(strings.ToLower(cleverSentence), "")
 	for index, char := range characters {
 		if index%2 == 0 {
 			retardedSentence += strings.ToUpper(char)
